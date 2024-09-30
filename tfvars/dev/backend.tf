f6c2278cb944314f18f34128c1a7c922d25b41c6
kolavn <veda.kola@gmail.com>
2024-09-30 13:50:10 -0400

    Terraform in Multi-Env

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "veda-dev-bucket"
+key = "tfvars-demo-dev" 
+region = "us-east-1"
+dynamodb_table = "veda-dev-lock" 
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "veda-prod-bucket"
+key = "dfvars-demo-prod" 
+region = "us-east-1"
+dynamodb_table = "veda-prod-lock"  
